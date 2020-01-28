2f7670678995468c9cdf5bdf587ed99c9956c908
songcai342000 <36162858+songcai342000@users.noreply.github.com>
2020-01-28 11:30:21 +0100

    Add files via upload

---
+++ Data/PersonManagementContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Data
+{
+    public class PersonManagementContext : DbContext
+    {
+        public PersonManagementContext(DbContextOptions<PersonManagementContext> options) : base(options)
+        {
+        }
+            public DbSet<Department> Department { get; set; }
+            public DbSet<Person> Person { get; set; }
+            public DbSet<Position> Position { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Person>().ToTable("Person");
+            modelBuilder.Entity<Position>().ToTable("Position");
+        }
+    }
+}
